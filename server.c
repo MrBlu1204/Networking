e791f5c52b834f14516e6ce7b5a69cefcdc5d7b6
Manish Ranjan Behera <manish12042000@gmail.com>
2020-10-12 16:40:55 +0530

    TCP Program LAB 5 Q3
    C program in TCP to send a string from client to server. On the server side, display the string first and then check if it is palindrome string.

--- server.c
+++ server.c
@@ -1,19 +1,51 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 
-int main()
-{ int ss,sb;
-  ss=socket(AF_INET,SOCK_DGRAM,0);
-  struct sockaddr_in servaddr;
-  servaddr.sin_family=AF_INET;
-  servaddr.sin_port=ntohs(6011);
-    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    bind(ss,(struct sockaddr *)&servaddr,sizeof(struct sockaddr));
-    struct sockaddr_in tempsendaddr;
+int main(){
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket < 0 ? printf("Error Opening Socket\n") : printf("Socket Working\n");
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(6001);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+    listen(server_socket, 5);
+    int client_socket = accept(server_socket, NULL, NULL);
+
     char buf[100];
-    int len=sizeof(struct sockaddr);
-    recvfrom(ss,buf,100,0,(struct sockaddr *)&tempsendaddr,&len);
-    printf("Receive from : %s",buf);
+    recv(client_socket, &buf, sizeof(buf), 0);
+    printf("Client : %s\n", buf);
+
+    int vowel = 0;
+    int len = strlen(buf);
+    char rev[100];
+
+    int end = len - 1;
+
+    for (int begin = 0; begin < len; begin++) {
+      rev[begin] = buf[end];
+      end--;
+    }
+
+    printf("Reverse : %s\n", rev);
+    int count = 0;
+    for (int i = 0 ; i < len -1; i++){
+        if(buf[i] == rev[i]){
+            count++;
+        }
+    }
+    if (count == len - 1){
+        printf("Entered String is PALINDROME\n");
+    } else {
+        printf("Entered String is NOT PALINDROME\n");
+    }
+
+    return 0;
 }
