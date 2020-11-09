3acdc6df3ca460f63cd51533c54f0ab1e3bf6f8a
Manish Ranjan Behera <manish12042000@gmail.com>
2020-11-09 16:16:45 +0530

    Capitalize the first Alphabet of every Word
    Program to send a string from client to server using TCP, server will convert the first character of each word to upper case and display the string in server side.

---
+++ CapitalizationServer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(){
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket < 0 ? printf("Error Opening Socket\n") : printf("Socket Working\n");
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(6001);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+    listen(server_socket, 5);
+    int client_socket = accept(server_socket, NULL, NULL);
+
+    char buf[100];
+    recv(client_socket, &buf, sizeof(buf), 0);
+    printf("Client : %s\n", buf);
+
+    int vowel = 0;
+    int len = strlen(buf);
+    for(int i=0; i<len;i++){
+       if(buf[i]>='a'&& buf[i]<='z'){
+           if(buf[i-1]==' ')
+              buf[i]=buf[i]-32;
+        }
+     }
+     
+     printf("New Capitalized String: %s\n",buf);
+    return 0;
+}
